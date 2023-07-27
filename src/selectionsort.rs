2cb97ef50e9d122445087057d0ae81c475b95827
laureanray <laureanraybahala@gmail.com>
2023-07-27 13:45:04 +0800

    add selection sort

---
+++ src/selectionsort.rs
@@ -0,0 +1,37 @@
+use super::Sorter;
+
+pub struct SelectionSort;
+
+impl Sorter for SelectionSort {
+    fn sort<T>(&self, slice: &mut [T])
+    where
+        T: Ord,
+    {
+        for unsorted in 0..slice.len() {
+            let (smallest_in_rest, _) = slice[unsorted..]
+                .iter()
+                .enumerate()
+                .min_by_key(|&(_, v)| v)
+                .expect("slice is non-empty");
+
+            // or
+            let mut smallest_in_rest = unsorted;
+            for i in (unsorted + 1)..slice.len() {
+                if slice[i] < slice[smallest_in_rest] {
+                    smallest_in_rest = i;
+                }
+            }
+
+            if unsorted != smallest_in_rest {
+                slice.swap(unsorted, smallest_in_rest);
+            }
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let mut things = vec![4, 2, 3, 1];
+    SelectionSort.sort(&mut things);
+    assert_eq!(things, &[1, 2, 3, 4]);
+}
