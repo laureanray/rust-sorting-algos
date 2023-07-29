af72b78fd4ccfb37617fae835b4efde219ee8e17
laureanray <laureanraybahala@gmail.com>
2023-07-29 16:22:51 +0800

    test passing

--- src/quicksort.rs
+++ src/quicksort.rs
@@ -1,30 +1,59 @@
 use super::Sorter;
 
-pub struct SelectionSort;
+pub struct QuickSort;
 
-impl Sorter for SelectionSort {
+fn quicksort<T: Ord>(slice: &mut [T]) {
+    match slice.len() {
+        0 | 1 => return,
+        2 => {
+            if slice[0] > slice[1] {
+                slice.swap(0, 1);
+            }
+            return;
+        }
+        _ => {}
+    }
+
+    let (pivot, rest) = slice.split_first_mut().expect("slice is non-empty");
+    let mut left = 0;
+    let mut right = rest.len() - 1;
+    while left <= right {
+        if &rest[left] <= pivot {
+            // already on the correct side
+            left += 1;
+        } else if &rest[right] > pivot {
+            if right == 0 {
+                break;
+            }
+            right -= 1;
+        } else {
+            // move element to the right side
+            rest.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
+    }
+    let left = left + 1;
+
+    slice.swap(0, left - 1);
+    let (left, right) = slice.split_at_mut(left - 1);
+    assert!(left.last() <= right.first());
+    quicksort(left);
+    quicksort(&mut right[1..]);
+}
+
+impl Sorter for QuickSort {
     fn sort<T>(&self, slice: &mut [T])
     where
         T: Ord,
     {
-        for unsorted in 0..slice.len() {
-            let smallest_in_rest = slice[unsorted..]
-                .iter()
-                .enumerate()
-                .min_by_key(|&(_, v)| v)
-                .map(|(i, _)| unsorted + i)
-                .expect("slice is non-empty");
-
-            if unsorted != smallest_in_rest {
-                slice.swap(unsorted, smallest_in_rest);
-            }
-        }
+        quicksort(slice);
     }
 }
 
 #[test]
 fn it_works() {
     let mut things = vec![4, 2, 3, 1];
-    SelectionSort.sort(&mut things);
+    QuickSort.sort(&mut things);
     assert_eq!(things, &[1, 2, 3, 4]);
 }
